2efa3c11e6d812c3426fb26ce2b2d216af7e2edf
Ganesh31088500 <ganeshpitchuka8@gmail.com>
2025-08-24 13:11:49 +0530

    first commit

---
+++ SpringBootAPIs/src/test/java/com/example/SpringBootAPIs/SpringBootApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
